7a60020c7b6af60d976022e932e5eb144d0557e2
Breanna Yang <breanna.yang1@gmail.com>
2024-07-28 04:03:48 -0500

    Working on Scene 2

--- BillboardHot100Clean.py
+++ BillboardHot100Clean.py
@@ -34,3 +34,26 @@ all_songs_genres = all_songs_genres[all_songs_genres['Genres'].isin(genres)]
 all_songs_genres = all_songs_genres.groupby(['Year', 'Genres']).size().reset_index(name='Unique_Songs')
 
 all_songs_genres.to_csv('Scene1Data.csv')
+
+# Scene 2: reorganize to get info for top 10 artists of all time
+all_songs['WeekID'] = pd.to_datetime(all_songs['WeekID'])  # Ensure WeekID is in datetime format
+# print(all_songs.head(10))
+
+unique_weeks = all_songs[['Performer_x', 'WeekID']].drop_duplicates()
+artist_week_count = unique_weeks.groupby('Performer_x')['WeekID'].nunique().reset_index()
+artist_week_count.columns = ['Performer_x', 'total_weeks']
+
+top_10_artists = artist_week_count.nlargest(10, 'total_weeks')['Performer_x']
+
+top_10_data = all_songs[all_songs['Performer_x'].isin(top_10_artists)]
+
+top_10_data['Year'] = pd.to_datetime(top_10_data['WeekID']).dt.year
+
+highest_rank_per_year = top_10_data.groupby(['Performer_x', 'Year']).apply(
+    lambda x: x.loc[x['Peak Position'].idxmin()]
+).reset_index(drop=True)
+
+highest_rank_per_year = highest_rank_per_year.merge(artist_week_count, on='Performer_x')
+
+final_table = highest_rank_per_year[['Performer_x', 'Year', 'Peak Position', 'Song_x', 'total_weeks']]
+final_table.to_csv('Scene2Data.csv')
